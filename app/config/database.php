56c484094a63f6595013be3752e12efedab4e496
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2025-10-10 09:40:34 +0200

    Eerste comit
    Templates html in de project gekregen
    Database file gemaakt en getest

---
+++ app/config/database.php
@@ -0,0 +1,26 @@
+<?php
+    $db_server = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "proweto";
+
+    // Create connection
+    $conn = "";
+
+
+
+       try{
+        $conn = mysqli_connect($db_server,
+        $db_username,
+         $db_password,
+          $db_name);
+       }catch(mysqli_sql_exception $e){
+          echo "Connection failed: " . $e->getMessage();
+         }
+         
+       if($conn){
+        echo "You are connected!";
+       }
+       
+
+?>
